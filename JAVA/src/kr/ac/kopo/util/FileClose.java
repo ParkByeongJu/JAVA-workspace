c72828699825d85ddd59eccfda0b4923043d51b4
ParkByeongJu <bj4906@gmail.com>
2023-04-12 13:39:04 +0900

    4월 12일 수업내용(IO API)

--- JAVA/src/kr/ac/kopo/util/FileClose.java
+++ JAVA/src/kr/ac/kopo/util/FileClose.java
@@ -2,6 +2,8 @@ package kr.ac.kopo.util;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 
 
 public class FileClose {
@@ -34,5 +36,35 @@ public class FileClose {
 	public static void close(OutputStream bos, OutputStream os) {
 		close(bos);
 		close(os);
+	}
+	
+	public static void close(Writer w) {
+		try {
+			if(w != null) {
+				w.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Reader r) {
+		try {
+			if(r != null) {
+				r.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Writer bw, Writer w) {
+		close(bw);
+		close(w);
+	}
+	
+	public static void close(Reader br, Reader r) {
+		close(br);
+		close(r);
 	}
 }
